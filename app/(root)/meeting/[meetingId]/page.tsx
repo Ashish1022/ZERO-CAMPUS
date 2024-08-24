dde12afd327bb1664d2b52083534942c233d7ed5
Ashish <ashishjadhav3300@gmail.com>
2024-08-24 23:32:44 +0530

    custom usercall hook

--- app/(root)/meeting/[meetingId]/page.tsx
+++ app/(root)/meeting/[meetingId]/page.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+"use client"
+
+
+import MeetingRoom from '@/components/MeetingRoom'
+import MeetingSetup from '@/components/MeetingSetup'
+import { useUser } from '@clerk/nextjs'
+import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
 
 const page = ({params}:{params:{meetingId:string}}) => {
+
+  const {user, isLoaded} = useUser();
+
+  const [isSetupComplete, setIsSetupComplete] = useState(false)
+
   return (
-    <div className='text-white'>meeting details for {params.meetingId}</div>
+    <main className='h-screen w-full'>
+      <StreamCall>
+        <StreamTheme>
+          {isSetupComplete ? (
+            <MeetingSetup/>
+          ):(
+            <MeetingRoom/>
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
   )
 }
 
