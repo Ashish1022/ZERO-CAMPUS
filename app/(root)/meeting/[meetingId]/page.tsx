e30681feda6057701d6df4f7531d7ed887bcbcbc
Ashish <ashishjadhav3300@gmail.com>
2024-08-25 10:22:24 +0530

    meeting room setup

--- app/(root)/meeting/[meetingId]/page.tsx
+++ app/(root)/meeting/[meetingId]/page.tsx
@@ -1,24 +1,30 @@
 "use client"
 
 
+import Loader from '@/components/Loader'
 import MeetingRoom from '@/components/MeetingRoom'
 import MeetingSetup from '@/components/MeetingSetup'
+import { useGetCallById } from '@/hooks/useGetCallById'
 import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk'
 import React, { useState } from 'react'
 
-const page = ({params}:{params:{meetingId:string}}) => {
+const page = ({params:{meetingId}}:{params:{meetingId:string}}) => {
 
   const {user, isLoaded} = useUser();
 
-  const [isSetupComplete, setIsSetupComplete] = useState(false)
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+  const {call ,isCallLoading} = useGetCallById(meetingId);
+
+  if(!isLoaded || isCallLoading) return <Loader/>
 
   return (
     <main className='h-screen w-full'>
-      <StreamCall>
+      <StreamCall call={call}> 
         <StreamTheme>
-          {isSetupComplete ? (
-            <MeetingSetup/>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete}/>
           ):(
             <MeetingRoom/>
           )}
